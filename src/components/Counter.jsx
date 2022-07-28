5a0b955b497bf82ce115f4a00fa4390e76e6682f
ahcoo <mynameisjins@gmail.com>
2022-07-28 18:35:03 +0900

    220728 react lecture

---
+++ src/components/Counter.jsx
@@ -0,0 +1,33 @@
+/*import React, { useState } from "react";
+
+const Counter = ({ counter, setCounter }) => {
+  const addCounter = () => {
+    setCounter(counter + 1);
+  };
+  return (
+    <div>
+      <div>{counter}번 클릭했습니다</div>
+      <button onClick={addCounter}>+</button>
+    </div>
+  );
+  return;
+};
+
+export default Counter;*/
+
+import React, { useState } from "react";
+
+function Counter({ counter, setCounter }) {
+  const addCounter = () => {
+    setCounter(counter + 1);
+  };
+  return (
+    <div>
+      <div>{counter}번 클릭했습니다</div>
+      <button onClick={addCounter}>+</button>
+    </div>
+  );
+  return;
+}
+
+export default Counter;
